9bf2e35eaad277f9c5104f02c3a551a49c5c421c
Vitória Barros <108809563+vitoriabarros132@users.noreply.github.com>
2025-05-05 08:07:04 -0300

    Sensor de distância ultrassônico
    Componentes utilizados:
    - 1 Arduino Uno R3
    - 1 Sensor de distância ultrassônico
    - 1 RCBG LED RGB
    - 3 220 Ω Resistor

---
+++ sensor_de_dist_ncia1.ino
@@ -0,0 +1,67 @@
+const int verde = 3;
+const int vermelho = 5;
+const int azul = 4;
+
+const int sensorPin = 6;
+
+void setup()
+{
+  //Serial.begin(9600);
+  pinMode(vermelho, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(azul, OUTPUT);
+  
+  pinMode(sensorPin, OUTPUT);
+}
+
+long medirDistancia()
+{
+  //Lançar o pulso
+  pinMode(sensorPin, OUTPUT);
+  digitalWrite(sensorPin, LOW);
+  delay(2);
+  digitalWrite(sensorPin, HIGH);
+  delay(10);
+  
+  //Ler o pulso (somente valor HIGH)
+  pinMode(sensorPin, INPUT);
+  long duracao = pulseIn(sensorPin, HIGH);
+  //0.034 cm/ms - velocidade do som; divide por 2 pra ter a ida
+  //retorna em cm
+  long distancia = duracao * 0.034 / 2;
+  
+  return distancia;
+}
+
+void loop()
+{
+  long distancia = medirDistancia();
+  Serial.print("Distancia: ");
+  Serial.print(distancia);
+  Serial.print(" cm");
+  
+  //limite mínimo = 20cm; (azul)
+  //médio - entre 21 e 160; (verde)
+  //limite máximo = 160cm (vermelho)
+  
+  if (distancia <= 20)
+  {
+    digitalWrite(vermelho, LOW);
+    digitalWrite(verde, LOW);
+    digitalWrite(azul, HIGH);
+  }
+  else if (distancia >= 160)
+  {
+    digitalWrite(vermelho, HIGH);
+    digitalWrite(verde, LOW);
+    digitalWrite(azul, LOW);
+  }
+  else
+  {
+    digitalWrite(vermelho, LOW);
+    digitalWrite(verde, HIGH);
+    digitalWrite(azul, LOW);
+  }
+  
+  delay(1000);
+}
